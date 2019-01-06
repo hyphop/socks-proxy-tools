d5040b9142c8f89d13c6ae0623f3220daa3860dc
hyphop <email2tema@gmail.com>
2019-01-06 14:52:08 +0700

    Sun Jan  6 14:52:08 +07 2019

--- runas/runas.make
+++ runas/runas.make
@@ -8,7 +8,16 @@ echo "[i] $0">&2
 
 gcc runas.c -o ../bin/runas && {
 
-    ./runas.prepare || {
+    ./runas.prepare ../bin/runas || {
+	echo "[i] run via SUDO" >&2
+	sudo ./runas.prepare
+    }
+
+}
+
+gcc runasg.c -o ../bin/runasg && {
+
+    ./runas.prepare ../bin/runasg || {
 	echo "[i] run via SUDO" >&2
 	sudo ./runas.prepare
     }
--- runas/runasg.c
+++ runas/runasg.c
@@ -7,36 +7,20 @@
 
    int main(int argc, char* argv[], char** envp)
    {
-
-   int uid = 0;
-
-
-
    char *gs;
-   char *us;
-
-   int gid = 0;
-
+   int gid = 0 ;
 
    gs = getenv("GID");
-   us = getenv("UID");
-
-   if (us) {
-      uid=atoi(us);
-   }
 
    if (gs) {
-      gid=atoi(gs);
+	gid=atoi(gs);
    }
 
-
-   setuid(uid);
    setgid(gid);
-
-   fprintf(stderr, "%s UID: %d, GID: %d\n", "runas", uid, gid);
-
-
    argv++;
+
+   fprintf(stderr, "%s GID: %d\n", "runasg", gid);
+
    execve(argv[0], argv, envp);
    perror("execve");   /* execve() returns only on error */
    exit(EXIT_FAILURE);
